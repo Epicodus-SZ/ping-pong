5d7295cc140ec74e62280e7933b483cfee426b91
Steve Zaske <github@zaske.com>
2017-06-09 10:19:17 -0700

    include working array replacement logic

--- js/script.js
+++ js/script.js
@@ -1,3 +1,29 @@
-$(document).ready(function() {
+//BIZ logic here
+function evalNum(num){
+  if (num%15===0) return "ping-pong";
+  if (num%5===0) return "pong";
+  if (num%3===0) return "ping";
+  return num;
+}
 
+function pingPong(num){
+  return Array.from(Array(num)
+              .keys())
+              .map(x => ++x) //arrow functions! .map(function(event){return ++event;});
+              .map(x => evalNum(x));
+}
+
+
+
+//UI logic here
+$(document).ready(function() {
+  $("#form").submit(function(event){
+    event.preventDefault();
+
+    displayResults(pingPong(parseInt($("#inputPong").val())));
+  }); // end of submit listener
 });
+
+function displayResults(resultArray){
+  $("#results").text(resultArray);
+}
